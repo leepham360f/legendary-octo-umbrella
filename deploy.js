8a773ae664d9eaa62655bb3b1a35f4f3c73f0346
Lee Pham <lee.pham@360f.com>
2021-03-04 23:00:30 +0700

    update deploy script

--- deploy.js
+++ deploy.js
@@ -1,9 +1,6 @@
 const { execSync } = require("child_process");
 
-let tag = execSync(`git tag -l | grep $(git describe HEAD)`).toString().trim();
-if(!tag){
-  tag = execSync(`git describe --tags --abbrev=0`).toString().trim(); // could be increasing version later
-}
+const tag = execSync(`git describe --tags --abbrev=0`).toString().trim();
 console.log('Current tag', tag);
 const imageName = `hienpham95/demo-deploy:${tag}`;
 execSync(`docker build -t ${imageName} .`);
@@ -12,6 +9,6 @@ console.log('run docker login');
 const dockerhub = execSync(`echo "${process.env.DOCKER_USERNAME}" | docker login -u ${process.env.DOCKER_PASSWORD} --password-stdin`).toString().trim();
 console.log(dockerhub);
 const pushImage = execSync(`docker push ${imageName}`).toString().trim();
-console.log(pushImage);
+console.log('Finished pushing image', pushImage);
 
 // git tag -a da-v1.0.0.dev -m "tag version"
