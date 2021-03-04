e8b3afa84ba9f424aa4349516b292c319460b490
Lee Pham <lee.pham@360f.com>
2021-03-05 00:18:00 +0700

    xz

--- deploy.js
+++ deploy.js
@@ -6,6 +6,7 @@ const getTag = () => {
     return execSync(`git tag -l | grep $(git describe HEAD)`).toString().trim();
   } catch (error) {
     console.log(error);
+    return 'da-v1.0.0.dev';
   }
 }
 
@@ -15,8 +16,9 @@ const imageName = `hienpham95/demo-deploy:${tag}`;
 execSync(`docker build -t ${imageName} .`);
 console.log('image built successfully');
 console.log('run docker login');
-const dockerhub = execSync(`echo "${process.env.DOCKER_PASSWORD}" | docker login -u "${process.env.DOCKER_USERNAME}" --password-stdin`).toString().trim();
-console.log(dockerhub);
+// const dockerhub = execSync(`echo "${process.env.DOCKER_PASSWORD}" | docker login -u "${process.env.DOCKER_USERNAME}" --password-stdin`).toString().trim();
+execSync(`docker login -u ${process.env.DOCKER_USERNAME} -p ${process.env.DOCKER_PASSWORD}`);
+// console.log(dockerhub);
 const pushImage = execSync(`docker push ${imageName}`).toString().trim();
 console.log('Finished pushing image', pushImage);
 
