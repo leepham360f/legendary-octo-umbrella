5a5572a47a3526adaa68febfd1d7c359261bce08
Lee Pham <lee.pham@360f.com>
2021-03-04 21:06:51 +0700

    make project to run on Docker

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+app.use(bodyParser.json())
+
+const posts = {}
+
+
+app.get("/", (req, res) => {
+  res.send({
+    data: 'hello world'
+  })
+})
+
+app.listen(4002, async () => {
+  console.log("Listening on 4002")
+})
