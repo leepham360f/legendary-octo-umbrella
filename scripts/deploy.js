4e7f884c86119fd1be34e125db360dba7a538040
Lee Pham <lee.pham@360f.com>
2021-03-05 12:54:39 +0700

    test

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,31 @@
-const { execSync } = require("child_process");
+const { execSync } = require('child_process')
 
 const getTag = () => {
   try {
-    execSync(`git fetch --tags -f`);
-    let tag = execSync(`git tag -l | grep $(git describe HEAD)`).toString().trim();
-    const currentBranch = execSync(`git rev-parse --abbrev-ref HEAD`);
-    if(currentBranch !== 'dev'){
-      tag = tag.replace('dev', currentBranch);
+    execSync(`git fetch --tags -f`)
+    let tag = execSync(`git tag -l | grep $(git describe HEAD)`)
+      .toString()
+      .trim()
+    const currentBranch = execSync(`git rev-parse --abbrev-ref HEAD`)
+    if (currentBranch !== 'dev') {
+      tag = tag.replace('dev', currentBranch)
     }
-  } catch (error) {}
+  } catch (error) {
+    return execSync(`git describe --tags --abbrev=0`)
+  }
 }
 
-const tag = getTag();
-if(!tag){
-  console.log('No tag found.');
-  return;
+const tag = getTag()
+if (!tag) {
+  console.log('No tag found.')
+  return
 }
 
-console.log('Current tag', tag);
-const imageName = `hienpham95/demo-deploy:${tag}`;
-execSync(`docker build -t ${imageName} .`);
-console.log('Image built successfully');
-const pushImage = execSync(`docker push ${imageName}`).toString().trim();
-console.log('Finished pushing image', pushImage);
+console.log('Current tag', tag)
+const imageName = `hienpham95/demo-deploy:${tag}`
+execSync(`docker build -t ${imageName} .`)
+console.log('Image built successfully')
+const pushImage = execSync(`docker push ${imageName}`).toString().trim()
+console.log('Finished pushing image', pushImage)
 
 // git tag -a fpro-v1.0.0.dev -m "tag version"
