1cf77909dc15d4fa4b2d063c455b7327c9428ae0
Lee Pham <lee.pham@360f.com>
2021-03-05 14:28:03 +0700

    update tag

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,34 @@
 const { execSync } = require('child_process')
 
+const pushTag = (tag) => {
+  execSync(`git tag -a ${tag} -m "Tag."`)
+  execSync(`git push origin ${tag}`)
+}
+
 const getTag = () => {
   let tag
+  const currentBranch = execSync(`git rev-parse --abbrev-ref HEAD`).toString().trim()
   try {
     execSync('git fetch --unshallow')
     tag = execSync(`git tag -l | grep $(git describe HEAD)`).toString().trim()
   } catch (error) {
     // get lastest tag of current branch
     tag = execSync(`git describe --abbrev=0`).toString().trim()
-    const minorVersionIndex = tag.indexOf('-v') + 6
-    let currentMinorVersion = tag.charAt(minorVersionIndex)
 
-    // auto increase minor version
-    tag = tag.split('')
-    tag[minorVersionIndex] = Number(currentMinorVersion) + 1
-    tag = tag.join('')
+    // auto increase minor version for dev branch
+    if (currentBranch === 'dev') {
+      const minorVersionIndex = tag.indexOf('-v') + 6
+      const currentMinorVersion = tag.charAt(minorVersionIndex)
+      tag = tag.split('')
+      tag[minorVersionIndex] = Number(currentMinorVersion) + 1
+      tag = tag.join('')
+      pushTag(tag)
+    }
   }
-
-  const currentBranch = execSync(`git rev-parse --abbrev-ref HEAD`)
-    .toString()
-    .trim()
+  
   if (currentBranch !== 'dev') {
     tag = tag.replace('dev', currentBranch)
-    execSync(`git tag -a ${tag} -m "Tag UAT release."`)
-    execSync(`git push origin ${tag}`)
+    pushTag(tag)
   }
 
   return tag
