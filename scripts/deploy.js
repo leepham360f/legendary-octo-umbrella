1c927f51d7422a4bf087e06f9bbb2803d73111a2
Lee Pham <lee.pham@360f.com>
2021-03-05 13:37:51 +0700

    test auto increase version

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,36 @@
 const { execSync } = require('child_process')
 
 const getTag = () => {
+  let tag
   try {
     execSync('git fetch --unshallow')
-    let tag = execSync(`git tag -l | grep $(git describe HEAD)`)
-      .toString()
-      .trim()
-    const currentBranch = execSync(`git rev-parse --abbrev-ref HEAD`)
-    if (currentBranch !== 'dev') {
-      tag = tag.replace('dev', currentBranch)
-    }
+    tag = execSync(`git tag -l | grep $(git describe HEAD)`).toString().trim()
   } catch (error) {
-    return execSync(`git describe --abbrev=0`).toString().trim();
+    // get lastest tag of current branch
+    tag = execSync(`git describe --abbrev=0`).toString().trim()
+    const minorVersionIndex = tag.indexOf('-v') + 6
+    let currentMinorVersion = tag.charAt(minorVersionIndex)
+    tag = tag.split('')
+    tag[minorVersionIndex] = Number(currentMinorVersion) + 1
+    tag = tag.join('')
   }
+
+  const currentBranch = execSync(`git rev-parse --abbrev-ref HEAD`)
+  if (currentBranch !== 'dev') {
+    tag = tag.replace('dev', currentBranch)
+  }
+
+  return tag
 }
 
 const tag = getTag()
+console.log(tag)
 if (!tag) {
   console.log('No tag found.')
   return
 }
-
-console.log('Current tag', tag)
 const imageName = `hienpham95/demo-deploy:${tag}`
 execSync(`docker build -t ${imageName} .`)
-console.log('Image built successfully')
 const pushImage = execSync(`docker push ${imageName}`).toString().trim()
 console.log('Finished pushing image', pushImage)
 
