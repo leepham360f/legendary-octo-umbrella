41e5e2436fec05aabd57802c215365e41f6c2a13
Lee Pham <lee.pham@360f.com>
2021-03-10 18:27:59 +0700

    test github 10

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,5 +2,5 @@
 echo "hello world"
 # exec git tag -l | grep $(git describe HEAD)
 # exec git describe --tags --abbrev=0
-echo ::set-output name=tag_version::$(echo $RANDOM)
-echo tag_version
+# echo ::set-output name=tag_version::$(echo $RANDOM)
+echo ::set-output name=tag_version::$(echo $(git describe HEAD))
