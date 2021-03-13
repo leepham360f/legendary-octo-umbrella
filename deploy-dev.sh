d7338bf9577e7c37f8e59cb4e06efdf03c7d6f87
Lee Pham <lee.pham@360f.com>
2021-03-13 12:35:21 +0700

    tmp

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "hello world"
+echo "hello world !"
 # exec git tag -l | grep $(git describe HEAD)
 # exec git describe --tags --abbrev=0
 # echo ::set-output name=tag_version::$(echo $RANDOM)
